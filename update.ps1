077415c05810ea3d752bf3706c8316e39e03a233
JacobK233811 <66180976+JacobK233811@users.noreply.github.com>
2020-12-23 20:50:57 -0500

    Update update.ps1
    Get-Content command to properly access checker and nc.py

--- update.ps1
+++ update.ps1
@@ -1,11 +1,8 @@
 cd c:\
 $url = 'https://github.com/JacobK233811/MangaNewChapter/archive/main.zip'
-New-Item -ItemType Directory Zips
 $Dest = 'C:\Zips\MQ.zip'
 $web = New-Object -TypeName System.Net.WebClient
 $web.DownloadFile($url, $Dest)
-
-New-Item -ItemType Directory Extracts
 $ExtractDir = 'C:\Extracts'
 
 $ExtShell = New-Object -ComObject Shell.Application
@@ -14,5 +11,5 @@ $ExtShell.Namespace($ExtractDir).CopyHere($file)
 cd Extracts
 Rename-Item -Path .\MangaNewChapter-main .\MQuicker-update-folder
 cd MQuicker-update-folder
-checker.py | Set-Content ..\MQuicker\checker.py
-nc.py | Set-Content ..\MQuicker\nc.py
+Get-Content checker.py | Set-Content ..\MQuicker\checker.py
+Get-Content nc.py | Set-Content ..\MQuicker\nc.py
