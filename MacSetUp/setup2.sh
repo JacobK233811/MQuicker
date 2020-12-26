29e36d6d1b4da6a806e2973ed3622705574c2135
JacobK233811 <66180976+JacobK233811@users.noreply.github.com>
2020-12-26 18:11:24 -0500

    Organizing SetUp
    Renamed SetUp to PythonFiles because that's what goes in there.
    
    Created (Windows\Mac)SetUp folders to separate all the .ps1 and .sh files along with their respective shortcuts.

--- MacSetUp/setup2.sh
+++ MacSetUp/setup2.sh
@@ -1,9 +1,9 @@
-cd "c:/MQuicker"
+cd "c:/MQuicker/MacSetUp"
 pip install virtualenv==20.0.31
 virtualenv mq
 source "./mq/Scripts/activate"
 echo "Please exercise patience as the following packages install."
-pip install -r requirements.txt
-mv "./MQuicker - For Mac Desktop.lnk" ($HOME + "/Desktop")
+pip install -r "../requirements.txt"
+mv "./MQuicker - For Desktop.lnk" ($HOME + "/Desktop")
 echo "Congratulations on Setting Up MQuicker!!"
 python checker.py
---
+++ WindowsSetUp/update.ps1
@@ -0,0 +1,15 @@
+# cd c:\
+# $url = 'https://github.com/JacobK233811/MangaNewChapter/archive/main.zip'
+# $Dest = 'C:\Zips\MQ.zip'
+# $web = New-Object -TypeName System.Net.WebClient
+# $web.DownloadFile($url, $Dest)
+# $ExtractDir = 'C:\Extracts'
+
+# $ExtShell = New-Object -ComObject Shell.Application
+# $file = $ExtShell.Namespace($Dest).Items()
+# $ExtShell.Namespace($ExtractDir).CopyHere($file)
+# cd Extracts
+# Rename-Item -Path .\MangaNewChapter-main ".\MQuicker-update-folder"
+# cd MQuicker-update-folder
+# Get-Content checker.py | Set-Content ..\MQuicker\checker.py
+# Get-Content nc.py | Set-Content ..\MQuicker\nc.py
