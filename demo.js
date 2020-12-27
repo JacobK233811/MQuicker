a6b7024b792b6612dcd9038a854c83e309bc6224
JacobK233811 <66180976+JacobK233811@users.noreply.github.com>
2020-12-27 13:24:57 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -6,3 +6,61 @@ function copyMyText() {
      //copy the text to the clipboard
      document.execCommand("copy");
 }
+
+function AdvancedCopy(){
+     //the text that is to be copied to the clipboard
+     var theText = 'This is the text that we want to copy to the clipboard';
+ 
+     //create our hidden div element
+     var hiddenCopy = document.createElement('div');
+     //set the innerHTML of the div
+     hiddenCopy.innerHTML = theText;
+     //set the position to be absolute and off the screen
+     hiddenCopy.style.position = 'absolute';
+     hiddenCopy.style.left = '-9999px';
+ 
+     //check and see if the user had a text selection range
+     var currentRange;
+     if(document.getSelection().rangeCount > 0)
+     {
+          //the user has a text selection range, store it
+          currentRange = document.getSelection().getRangeAt(0);
+          //remove the current selection
+          window.getSelection().removeRange(currentRange);
+     }
+     else
+     {
+          //they didn't have anything selected
+          currentRange = false;
+     }
+ 
+     //append the div to the body
+     document.body.appendChild(hiddenCopy);
+     //create a selection range
+     var CopyRange = document.createRange();
+     //set the copy range to be the hidden div
+     CopyRange.selectNode(hiddenCopy);
+     //add the copy range
+     window.getSelection().addRange(CopyRange);
+ 
+     //since not all browsers support this, use a try block
+     try
+     {
+          //copy the text
+          document.execCommand('copy');
+     }
+     catch(err)
+     {
+          window.alert("Your Browser Doesn't support this! Error : " + err);
+     }
+     //remove the selection range (Chrome throws a warning if we don't.)
+     window.getSelection().removeRange(CopyRange);
+     //remove the hidden div
+     document.body.removeChild(hiddenCopy);
+ 
+     //return the old selection range
+     if(currentRange)
+     {
+          window.getSelection().addRange(currentRange);
+     }
+}
