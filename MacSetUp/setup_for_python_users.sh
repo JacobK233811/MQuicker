770908659b31d310123fdee46cacfc147f3354e2
JacobK233811 <66180976+JacobK233811@users.noreply.github.com>
2020-12-27 18:36:33 -0500

    Update setup_for_python_users.sh

--- MacSetUp/setup_for_python_users.sh
+++ MacSetUp/setup_for_python_users.sh
@@ -18,7 +18,7 @@ cd MQuicker
 cd $HOME/MQuicker/MacSetUp
 pip3 install virtualenv==20.0.31
 virtualenv mq
-source "./mq/Scripts/activate"
+source "./mq/bin/activate"
 python3 -m pip install -U pip
 echo -e "\033[1;33mPlease exercise patience as the following packages install."
 python3 -m pip install -r "../requirements.txt"
