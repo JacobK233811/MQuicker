492e5faf8a22495226ef3bdee58418b61e0b80f4
JacobK233811 <66180976+JacobK233811@users.noreply.github.com>
2020-12-28 23:55:40 -0500

    Update setup_for_python_users.sh

--- MacSetUp/setup_for_python_users.sh
+++ MacSetUp/setup_for_python_users.sh
@@ -1,14 +1,14 @@
 cd $HOME
 url='https://github.com/JacobK233811/MangaNewChapter/archive/MQuicker-minimal.zip'
 mkdir Zips
-Dest=$HOME/Zips
+Dest=$HOME/Zips/MQ.zip
 
 curl $url -L -o $Dest
 
 # mkdir Extracts
 ExtractDir=$HOME
 cd Zips
-unzip -q main.zip -d $ExtractDir
+unzip -q MQ.zip -d $ExtractDir
 # cd $ExtractDir
 cd ..
 rm -r Zips
@@ -20,7 +20,8 @@ pip3 install virtualenv==20.0.31
 virtualenv mq
 source "./mq/bin/activate"
 python3 -m pip install -U pip
-echo -e "\033[1;33mPlease exercise patience as the following packages install."
+python3 -m pip install -U setuptools
+echo -e "\033[1;33mPlease exercise patience as the following packages install.\033[0,0m"
 python3 -m pip install -r "../requirements.txt"
 mv "./launch.sh" $HOME
 echo "Congratulations on Setting Up MQuicker!!"
